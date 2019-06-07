a319cba0990a08a50da91a9c971c54700a49e547
jlobasso <jlobasso@gmail.com>
2019-06-07 15:27:15 -0300

    message es_419

--- popupWindows/popoup.js
+++ popupWindows/popoup.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("btn-detect").addEventListener("click", function() {
     var inputText = document.getElementById("text").value;
-    var title = chrome.i18n.getMessage("appDescription")
+    var title = chrome.i18n.getMessage("appDescription","description")
     alert(title);
     console.log(title)
 
