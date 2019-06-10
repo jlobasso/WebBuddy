d7b7e325e2d5ffcef53445492a9ff2fa4027ad77
Andres Bertuccio <abertuccio@gmail.com>
2019-06-10 12:40:59 -0300

    content

---
+++ content/content.js
@@ -0,0 +1,16 @@
+const iframe = document.createElement('iframe');
+iframe.src = chrome.extension.getURL('./content/pulpoNav.html');
+const height = "100px";
+iframe.style.height = height;
+iframe.style.width = '100%';
+iframe.style.position = 'fixed';
+iframe.style.top = '0';
+iframe.style.left = '0';
+iframe.style.zIndex = '938089';
+document.documentElement.appendChild(iframe);
+var bodyStyle = document.body.style;
+var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
+bodyStyle[cssTransform] = 'translateY(' + height + ')';
+
+
+
