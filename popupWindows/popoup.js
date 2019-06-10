b2ce0e592965904ce7e2437e94068ff1598e9779
jlobasso <jlobasso@gmail.com>
2019-06-10 15:45:12 -0300

    trad

--- popupWindows/popoup.js
+++ popupWindows/popoup.js
@@ -2,10 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("btn-detect").addEventListener("click", function() {
-    var inputText = document.getElementById("text").value;
-    var title = chrome.i18n.getMessage("appDescription","description")
-    alert(title);
-    console.log(title)
+    
+    console.log('HOLAAAAAAAAAA')
 
   });
+  
+});
+
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//   console.log('Turning ' + tab.url + ' red!');
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"'
+//   });
+// });
+
+
+
+chrome.runtime.onMessage.addListener(function(message, callback) {
+    if (message.data == "setAlarm") {
+        chrome.alarms.create({delayInMinutes: 5})
+      } else if (message.data == "runLogic") {
+        chrome.tabs.executeScript({file: 'logic.js'});
+      } else if (message.data == "changeColor") {
+        chrome.tabs.executeScript(
+            {code: 'document.body.style.backgroundColor="orange"'});
+      };
 });
