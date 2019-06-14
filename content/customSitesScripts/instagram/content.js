2b3bc001ab4fd2b1fe581f82f273811bad0b62e7
abertuccio <abertuccio@gmail.com>
2019-06-14 16:03:09 -0300

    maincontent behavior and hashtag instagram

--- content/customSitesScripts/instagram/content.js
+++ content/customSitesScripts/instagram/content.js
@@ -1,1 +1,18 @@
-console.log("estamos en INSTAGRAM");
+/************* DO NOT USE CONST HERE **********/
+/*THIS CONTENT AND VARS COULD BE OVERWRITTEN THROUGH EXECUTION*/
+
+
+if(!window.contentScriptInjected){
+    contentScriptInjected = true;
+
+    /* we define vars here, once*/
+    var hashtagElement = null;
+    var hashtagText = null;
+}
+
+hashtagElement = document.querySelectorAll("._7UhW9.fKFbl.yUEEX.KV-D4.uL8Hv");
+hashtagText = hashtagElement[0].innerText;
+
+//TODO: CHEQUEAR SI EL TAG YA ESTA AGREGADO
+//TODO: AGREGAR BOTON, DE YA AGREGADO O DE AGREGAR HASHTAG
+//TODO: COMPORTAMIENTO DE AGREGAR HASHTAG
--- content/mainContent.js
+++ content/mainContent.js
@@ -1,47 +1,58 @@
-const iframe = document.createElement('iframe');
-iframe.src = chrome.extension.getURL('./content/pulpoNav.html');
-const normalHeight = 100;
-const maxHeight = 190;
-iframe.id = "webBuddyByPulpou"
-document.documentElement.appendChild(iframe);
-const bodyStyle = document.body.style;
+/* THIS SCRIPT IS EXECUTED ONCE FOR TAB*/
+if(!window.contentScriptInjected){
+    contentScriptInjected = true;
+    const iframe = document.createElement('iframe');
+    iframe.src = chrome.extension.getURL('./content/pulpoNav.html');
+    iframe.id = "webBuddyByPulpou"
+    document.documentElement.appendChild(iframe);
 
-iframe.addEventListener("load", () => {
-    iframe.style.height = normalHeight+'px';
-    iframe.style.width = '101%';
-    iframe.style.position = 'fixed';
-    iframe.style.top = '0';
-    iframe.style.left = '0';
-    iframe.style.zIndex = '938089';
-    iframe.style.margin = '0px';
-    iframe.style.padding = '0px';
-    iframe.style.border = 'none';
-    bodyStyle.transform = 'translateY(' + normalHeight +'px )';
-});
+    const normalHeight = 100;
+    const maxHeight = 190;
+    const bodyStyle = document.body.style;
+    
+    
+    iframe.addEventListener("load", () => {
+        iframe.style.height = normalHeight+'px';
+        iframe.style.width = '101%';
+        iframe.style.position = 'fixed';
+        iframe.style.top = '0';
+        iframe.style.left = '0';
+        iframe.style.zIndex = '938089';
+        iframe.style.margin = '0px';
+        iframe.style.padding = '0px';
+        iframe.style.border = 'none';
+        bodyStyle.transform = 'translateY(' + normalHeight +'px )';
+    });
 
 
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-
-    if (message.target == "mainContent" && message.action === 'BAR_VISIBILITY') {
-
-        if (message.value === 'showBar') {
-            iframe.style.height = maxHeight +'px';
-            bodyStyle.transform = 'translateY(' + maxHeight +'px)';
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    
+        if (message.target == "mainContent" && message.action === 'BAR_VISIBILITY') {
+    
+            if (message.value === 'showBar') {
+                iframe.style.height = maxHeight +'px';
+                bodyStyle.transform = 'translateY(' + maxHeight +'px)';
+            }
+            if (message.value === 'hideBar') {
+                iframe.style.height = normalHeight +'px';
+                bodyStyle.transform = 'translateY(' + normalHeight +'px)';
+            }
+    
+            if (message.value === 'showSubMenuData') {
+                const fullHeight = +maxHeight + message.data.height;
+                iframe.style.height = fullHeight+'px';
+                bodyStyle.transform = 'translateY(' + fullHeight +'px)';
+            }
         }
-        if (message.value === 'hideBar') {
-            iframe.style.height = normalHeight +'px';
-            bodyStyle.transform = 'translateY(' + normalHeight +'px)';
-        }
+    
+    });
 
-        if (message.value === 'showSubMenuData') {
-            const fullHeight = +maxHeight + message.data.height;
-            iframe.style.height = fullHeight+'px';
-            bodyStyle.transform = 'translateY(' + fullHeight +'px)';
-        }
-    }
+}
 
-});
+
+
+
+
 
 
 
