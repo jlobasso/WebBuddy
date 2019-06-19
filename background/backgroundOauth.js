639be4690340c10530038b7abb4f76dc85ffff24
jlobasso <jlobasso@gmail.com>
2019-06-19 12:57:04 -0300

    popup login a backend

---
+++ background/backgroundOauth.js
@@ -0,0 +1,33 @@
+
+
+chrome.runtime.onInstalled.addListener(function() {  
+    var rule = {
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.instagram.com'}
+            })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    };   
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
+
+});
+
+
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+    if(message.action === 'LOGIN' && message.target === 'backOauth'){
+        sendResponse('LOGIN ENVIADO')
+        fetch('http://localhost:3000/get-menu')
+        .then(function(response) {
+            return response.json();
+        })
+    
+        .then(function(myJson) {
+            console.log(myJson);
+        });
+    }
+
+});
