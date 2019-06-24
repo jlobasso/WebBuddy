3b831781cd6bc850d04521ffb2fe90576736e4c5
abertuccio <abertuccio@gmail.com>
2019-06-24 09:54:22 -0300

    login refactor

--- background/background-login.js
+++ background/background-login.js
@@ -1,35 +1,49 @@
 
 
-chrome.runtime.onInstalled.addListener(function() {  
+chrome.runtime.onInstalled.addListener(function () {
     var rule = {
         conditions: [
             new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'www.instagram.com'}
+                pageUrl: { hostEquals: 'www.instagram.com' }
             }),
             new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'www.mercadolibre.com.ar'}
+                pageUrl: { hostEquals: 'www.mercadolibre.com.ar' }
             })
         ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        
-    };   
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+
+    };
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([rule]);
     });
 });
 
+/*TODO: HACER EL LOGIN Y GUARDAR PROFILE EN LOCAL STORAGE*/
+/*TODO: REVISAR SESSION? */
+
 
 chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
-    if(message.action === 'LOGIN' && message.target === 'backOauth'){
-        sendResponse('LOGIN ENVIADO')
-        fetch('http://localhost:3000/get-menu')
-        .then(function(response) {
-            return response.json();
-        })
-    
-        .then(function(myJson) {
-            console.log(myJson);
-        });
+
+    if (message.target === 'background-login') {
+
+        switch (message.action) {
+            case 'LOGIN':
+                sendResponse('LOGIN ENVIADO')
+                fetch('http://localhost:3000/get-menu')
+                    .then(function (response) {
+                        return response.json();
+                    })
+
+                    .then(function (myJson) {
+                        console.log(myJson);
+                    });
+
+                break;
+
+            default:
+                break;
+        }
+
     }
 
 });
